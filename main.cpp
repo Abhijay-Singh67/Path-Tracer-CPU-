9a88ab73e3403977fc32534dc8088ef109b2880b
Abhijay-Singh67 <abhijaysingh.1011@gmail.com>
2026-05-11 18:13:27 +0530

    Added hittable objects, lists and made the rendered handle ray collisions with nearest priority

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
-#include <iostream>
-#include "color.h"
-#include "vec3.h"
-#include "ray.h"
+#include "rtweekend.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-using namespace std;
-
-color ray_color(const ray& r){
+color ray_color(const ray& r, const hittable& world){
+    hit_record rec;
+    if(world.hit(r,0,infinity, rec)){
+        return 0.5*(rec.normal+color(1,1,1));
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
@@ -20,6 +22,12 @@ int main() {
     //Calculating the image height based on the aspect ratio, atleast 1
     int image_height = int(image_width/aspect_ratio);
     image_height = (image_height<1)?1:image_height;
+
+    //World
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0,0,-1),0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1),100));
 
     //Camera
 
@@ -42,9 +50,9 @@ int main() {
 
     //Render
 
-    cout << "P3\n";
-    cout << image_width << " " << image_height << "\n";
-    cout << "255\n";
+    std::cout << "P3\n";
+    std::cout << image_width << " " << image_height << "\n";
+    std::cout << "255\n";
 
     for (int j = 0; j < image_height; j++) {
         std::clog << "\rScanlines remaining: "<< (image_height-j) << " "<< std::flush;
@@ -54,10 +62,11 @@ int main() {
             
             ray r(camera_center, ray_direction);
             
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r,world);
             write_color(std::cout,pixel_color);
         }
     }
-    std::clog << "\rDone. \n";
+    std::clog<<"\rScanlines remaining: "<<0<<" "<<std::flush;
+    std::clog << "\nDone. \n";
     return 0;
 }
